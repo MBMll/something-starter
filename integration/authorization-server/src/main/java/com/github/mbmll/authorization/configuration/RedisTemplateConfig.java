90c63e7fe1625067c036351694ad37851a346a8d
aulor.xe <aulor.xe@outlook.com>
2024-02-05 02:34:14 +0800

    add deserializer

--- integration/authorization-server/src/main/java/com/github/mbmll/authorization/configuration/RedisTemplateConfig.java
+++ integration/authorization-server/src/main/java/com/github/mbmll/authorization/configuration/RedisTemplateConfig.java
@@ -1,5 +1,17 @@
 package com.github.mbmll.authorization.configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import com.github.mbmll.authorization.deserializer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +20,18 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import static com.github.mbmll.authorization.configuration.Constant.REDIS_TEMPLATE_REGISTERED_CLIENT_BEAN_NAME;
 
@@ -23,7 +46,9 @@ public class RedisTemplateConfig {
     /**
      * 注意：如果要使用注解 {@link Autowired} 管理 {@link RedisTemplate}， 则需要将 {@link RedisTemplate} 的
      * {@link Bean} 缺省泛型
+     *
      * @param redisConnectionFactory Redis 连接工厂
+     *
      * @return 返回 Redis 模板
      */
     @Bean(REDIS_TEMPLATE_REGISTERED_CLIENT_BEAN_NAME)
@@ -38,10 +63,9 @@ public class RedisTemplateConfig {
         template.setConnectionFactory(redisConnectionFactory);
 
         // 可以使用读写JSON
-        Jackson2JsonRedisSerializer<RegisteredClient> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(
-                RegisteredClient.class);
-
-//        jackson2JsonRedisSerializer.setObjectMapper(ObjectMapperUtils.redis());
+        Jackson2JsonRedisSerializer<RegisteredClient> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<>(redisObjectMapper(),
+                        RegisteredClient.class);
 
         // Redis 字符串：键、值序列化
         template.setKeySerializer(new StringRedisSerializer());
@@ -56,4 +80,63 @@ public class RedisTemplateConfig {
         return template;
     }
 
+    /**
+     * 标准时间格式：HH:mm:ss
+     */
+    public static final String NORM_TIME_PATTERN = "HH:mm:ss";
+
+    /**
+     * 标准日期格式：yyyy-MM-dd
+     */
+    public static final String NORM_DATE_PATTERN = "yyyy-MM-dd";
+
+    /**
+     * 标准日期时间格式，精确到秒：yyyy-MM-dd HH:mm:ss
+     */
+    public static final String NORM_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public static ObjectMapper redisObjectMapper() {
+        // ObjectMapper 提供了从基本 POJO（普通旧Java对象）或从通用 JSON 树模型（{@link JsonNode}）读取和写入 JSON
+        // 的功能，
+        // 以及执行转换的相关功能。
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // 枚举，定义影响Java对象序列化方式的简单开/关功能。
+        // 默认情况下启用功能，因此默认情况下日期/时间序列化为时间戳。
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        // 如果启用，上下文<code> TimeZone </
+        // code>将基本上覆盖任何其他TimeZone信息;如果禁用，则仅在值本身不包含任何TimeZone信息时使用。
+        objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
+
+        // 注册使用Jackson核心序列化{@code java.time}对象的功能的类。
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        // 添加序列化
+        // @formatter:off
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(NORM_DATETIME_PATTERN)));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(NORM_DATE_PATTERN)));
+        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(NORM_TIME_PATTERN)));
+        // @formatter:on
+
+        // 添加反序列化
+        // @formatter:off
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(NORM_DATETIME_PATTERN)));
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(NORM_DATE_PATTERN)));
+        javaTimeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(NORM_TIME_PATTERN)));
+        // @formatter:on
+
+        // 添加 OAuth 2.1 的反序列化
+        var simpleModule = new SimpleModule();
+        simpleModule.addDeserializer(ClientAuthenticationMethod.class, new ClientAuthenticationMethodDeserializer());
+        simpleModule.addDeserializer(AuthorizationGrantType.class, new AuthorizationGrantTypeDeserializer());
+        simpleModule.addDeserializer(ClientSettings.class, new ClientSettingsDeserializer());
+        simpleModule.addDeserializer(TokenSettings.class, new TokenSettingsDeserializer());
+        simpleModule.addDeserializer(OAuth2Authorization.class, new OAuth2AuthorizationDeserializer());
+        simpleModule.addDeserializer(OAuth2AuthorizationConsent.class, new OAuth2AuthorizationConsentDeserializer());
+
+        // 用于注册可以扩展该映射器提供的功能的模块的方法; 例如，通过添加自定义序列化程序和反序列化程序的提供程序。
+        objectMapper.registerModules(javaTimeModule, simpleModule);
+
+        return objectMapper;
+    }
 }
