9c834347c198bc025c749bc3860293f0e9c72e27
xialc <xialc@rayfaydata.com>
2024-03-23 20:42:02 +0800

    append file

---
+++ starter-free-example/rabbitmq-example/src/main/java/org/github/mbmll/example/rabbitmq/Main.java
@@ -0,0 +1,18 @@
+package org.github.mbmll.example.rabbitmq;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author xlc
+ * @Description
+ * @Date 2024/3/21 18:58
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
