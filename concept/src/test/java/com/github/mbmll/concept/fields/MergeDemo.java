595bffccefbad10e4ac6644290543b7e1535e02c
aulor.xe <aulor.xe@outlook.com>
2024-03-30 16:00:42 +0800

    add corrigendum

--- concept/src/test/java/com/github/mbmll/concept/fields/MergeDemo.java
+++ concept/src/test/java/com/github/mbmll/concept/fields/MergeDemo.java
@@ -1,6 +1,7 @@
 package com.github.mbmll.concept.fields;
 
 import lombok.Data;
+import org.junit.Test;
 
 import java.util.Date;
 
@@ -15,4 +16,15 @@ public class MergeDemo implements Time<Date>, Update<String, Date>, Create<Strin
     private Date createTime;
     private String updateId;
     private Date updateTime;
+
+    public <T extends Time<Date>> void convert(T target) {
+
+    }
+
+    @Test
+    public void test() {
+        MergeDemo mergeDemo = new MergeDemo();
+//        入参 和 形参 都要来自同一个接口,不能认为组合成的接口和被组合的接口完全相等
+        mergeDemo.convert(mergeDemo);
+    }
 }
